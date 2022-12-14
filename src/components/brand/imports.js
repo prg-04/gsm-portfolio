55a01558433c2fddd2e928d7eb21a3f8f048a3a9
prg-04 <karanjaevans95@gmail.com>
2022-12-14 17:36:17 +0300

    Add features and possibility components

---
+++ src/components/brand/imports.js
@@ -0,0 +1,14 @@
+import google from '../../assets/google.png'
+import slack from '../../assets/slack.png'
+import bitmap from '../../assets/Bitmap.png'
+import shopify from '../../assets/shopify.png'
+import dropbox from '../../assets/dropbox.png'
+
+export {
+	google,
+	slack,
+	dropbox,
+	bitmap,
+	shopify,
+}
+
